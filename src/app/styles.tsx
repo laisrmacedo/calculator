fe3a79c8cfb3150924656f78b45ab28e70dbc81e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2025-02-13 18:38:09 +0100

    style

---
+++ src/app/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: '100%'
+  },
+  containerBtn:{
+    width: '100%',
+    flexDirection: 'row',
+    // gap: 4,
+    justifyContent: 'space-around'
+    
+  }
+})
